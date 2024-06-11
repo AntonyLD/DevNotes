f5ce3a353eefde7fa9d557146a8b2fd4637c37d8
AntonyLD <antonyldcamargo@gmail.com>
2024-06-11 10:26:05 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,17 @@ const addNoteBtn = document.querySelector(".add-note");
 
 // Funções
 function showNotes() {
+    clearNotes()
+
     getNotes().forEach((note) =>{
         const noteElement = createNote(note.id, note.content, note.fixed);
 
         notesContainer.appendChild(noteElement)
-    })
+    });
+}
+
+function clearNotes() {
+    notesContainer.replaceChildren([])
 }
 
 
@@ -41,26 +47,80 @@ function generatedId() {
 
 function createNote(id, content, fixed){
 
-    const element = document.createElement("div")
+    const element = document.createElement("div");
 
-    element.classList.add("note")
+    element.classList.add("note");
 
-    const textarea = document.createElement("textarea")
+    const textarea = document.createElement("textarea");
 
     textarea.value = content
 
     textarea.placeholder = "Adicione Algum texto..."
 
-    element.appendChild(textarea)
+    element.appendChild(textarea);
+
+    const pinIcon = document.createElement("i");
+
+    pinIcon.classList.add(...["bi","bi-pin"]);
+
+    element.appendChild(pinIcon);
+
+    const deleteIcon = document.createElement("i");
+
+    deleteIcon.classList.add(...["bi","bi-x-lg"]);
+
+    element.appendChild(deleteIcon);
+
+    const duplicateIcon = document.createElement("i");
+
+    duplicateIcon.classList.add(...["bi","bi-file-earmark-plus"]);
+
+    element.appendChild(duplicateIcon);
+
+    if(fixed){
+        element.classList.add("fixed")
+    }
+
+    //eventos do elemento
+
+    element.querySelector(".bi-pin").addEventListener("click",() =>{
+        toggleFixNote(id)
+    });
+
+    element.querySelector(".bi-x-lg").addEventListener("click", () =>{
+        deleteNote(id, element)
+    });
 
     return element;
 }
 
+function toggleFixNote(id){
+    const notes = getNotes()
+
+    const targetNote = notes.filter((note) => note.id === id)[0]
+
+    targetNote.fixed = !targetNote.fixed;
+
+    saveNotes(notes);
+
+    showNotes()
+}
+
+function deleteNote(id, element){
+
+    const notes = getNotes().filter((note) => note.id !== id)
+
+    saveNotes(notes)
+
+    notesContainer.removeChild(element)
+}
 // Local Storage
 function getNotes(){
     const notes = JSON.parse(localStorage.getItem("notes") || "[]")
 
-    return notes;
+    const orderedNotes = notes.sort((a,b) => a.fixed > b.fixed ? -1 : 1)
+
+    return orderedNotes;
 }
 
 function saveNotes(notes){
